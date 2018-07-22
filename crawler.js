b493e1a0690d002307a97cb1f75d457a5178b651
sepiropht <sergembotta@mailoo.org>
2018-07-22 16:07:06 +0200

    working

--- crawler.js
+++ crawler.js
@@ -8,7 +8,7 @@ let stopInterval;
 module.exports = async ({ user, pass, courses, id }) => {
   console.log(chalk.green("You are using frontendmaster-downloader \n"));
   console.log(chalk.green("Try the login ... \n"));
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch{args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage();
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
